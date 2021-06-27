56facb629e92c353ec549040c2d717e9b4a99896
ga-sr <gabriel.10rocha@hotmail.com>
2021-06-26 22:42:28 -0300

    login hotfix

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -17,7 +17,7 @@ type RoomParams = {
 };
 
 export function Room() {
-  const { user } = useAuth();
+  const { user, signInWithGoogle } = useAuth();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState("");
   const roomId = params.id;
@@ -98,7 +98,10 @@ export function Room() {
               </div>
             ) : (
               <span>
-                Para enviar uma pergunta, <button>faça seu login.</button>
+                Para enviar uma pergunta,{" "}
+                <button onClick={() => signInWithGoogle()}>
+                  faça seu login.
+                </button>
               </span>
             )}
             <Button type="submit">Enviar pergunta</Button>
